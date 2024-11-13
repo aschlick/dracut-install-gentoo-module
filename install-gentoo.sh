becb10b8597cfec40ecb287bf17b07643f3dc999
Alexander Schlicker <alexander@schlickster.us>
2024-11-13 04:01:52 +0000

    Formatting fixes

--- install-gentoo.sh
+++ install-gentoo.sh
@@ -1,24 +1,46 @@
 #!/bin/sh
 
 function make_root_if_not_exists() {
+  local DEVICE=nothing
+  local TO_FORMAT=nothing
   ROOT_DEVICE=$(lsblk -lo NAME,PARTTYPE,FSTYPE | grep 4f68bce3-e8cd-4db1-96e7-fbcaf984b709 | grep xfs | cut -f1 -d" ")
-
-  if ! [ -b /dev/${ROOT_DEVICE} ]; then
-    sfdisk /dev/nvme0 << EOF
-,,L
-EOF
-    mkfs.xfs /dev/nvme0n1
+  if [ -z "${ROOT_DEVICE}" ]; then
+    if [ -b /dev/nvme0 ]; then
+      DEVICE=/dev/nvme0n1
+      TO_FORMAT=/dev/nvme0n1p1
+    elif [ -b /dev/sda ]; then
+      DEVICE=/dev/sda
+      TO_FORMAT=/dev/sda1
+    fi;
+  else
+    echo "Using root device ${ROOT_DEVICE}"
   fi;
 
-  mount ${ROOT_DEVICE} /
+  if ! [ -b /dev/${ROOT_DEVICE} ]; then
+    echo "Formatting ${DEVICE}"
+    sfdisk "${DEVICE}" << EOF
+label: gpt
+start=,size=,type=4f68bce3-e8cd-4db1-96e7-fbcaf984b709,name=ROOT
+EOF
+    echo "Making ${TO_FORMAT} xfs"
+    mkfs.xfs $TO_FORMAT
+
+    ROOT_DEVICE=$TO_FORMAT
+  fi;
+
+  echo "Mounting ${ROOT_DEVICE}"
+  mkdir -p /mnt/gentoo
+  mount ${ROOT_DEVICE} /mnt/gentoo
 }
 
 function copy_stage_4_if_no_portage() {
   wget schlicknas:/volume3/tftp/boot/stage4-amd64-systemd-local-v1.tar.xz
-  tar xpvf stage4-*.tar.xz --xattrs-include='*.*' --numeric-owner -C /
+  tar xpvf stage4-*.tar.xz --xattrs-include='*.*' --numeric-owner -C /mnt/gentoo
+  umount /mnt/gentoo
 }
 
 setup_everything() {
+  echo "Starting install-gentoo"
   local ROOT_DEVICE=nothing
   make_root_if_not_exists
   copy_stage_4_if_no_portage
--- module-setup.sh
+++ module-setup.sh
@@ -3,16 +3,26 @@
 check() {
   require_binaries wget || return 1
   require_binaries tar || return 1
+  require_binaries sfdisk || return 1
+  require_binaries lsblk || return 1
+  require_binaries cut || return 1
+  require_binaries grep || return 1
+  require_binaries mkfs.xfs || return 1
   return 0
 }
 
 depends() {
-  echo bash
+  echo bash network
   return 0
 }
 
 install() {
   inst /usr/bin/wget
   inst /usr/bin/tar
-  inst_hook pre-mount 91 "$moddir/install-gentoo.sh"
+  inst /usr/bin/sfdisk
+  inst /usr/bin/lsblk
+  inst /usr/bin/cut
+  inst /usr/bin/mkfs.xfs
+
+  inst_hook pre-trigger 91 "$moddir/install-gentoo.sh"
 }
